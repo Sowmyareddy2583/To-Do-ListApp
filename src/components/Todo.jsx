4be7a2ef8be9ffc0d0cfa00e26d321b506bd299e
Sowmyareddy2583 <127490161+Sowmyareddy2583@users.noreply.github.com>
2024-06-25 09:12:19 +0530

    Add feat done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    // console.log(inputText);
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       <div className="flex items-center mt-7 gap-2">
@@ -11,17 +24,27 @@ const Todo = () => {
       </div>
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          onClick={add}
+        >
           ADD +
         </button>
       </div>
       <div>
-        <TodoItems text="learn coding"/>
-        <TodoItems text="Create a portfilo"/>
+        {todoList.map((item, index) => (
+          <TodoItems
+            key={index}
+            id={item.id}
+            isCompleted={item.isCompleted}
+            text={item.text}
+          />
+        ))}
       </div>
     </div>
   );
